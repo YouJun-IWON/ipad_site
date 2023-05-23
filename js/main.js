52eb029578967e7c59d7bd583f14173d1bb79028
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2023-05-24 02:19:44 +0900

    Responsive layout work

--- js/main.js
+++ js/main.js
@@ -42,13 +42,17 @@ const searchInputEl = searchWrapEl.querySelector('input');
 const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
 
 searchStarterEl.addEventListener('click', showSearch);
-searchCloserEl.addEventListener('click', hideSearch);
+searchCloserEl.addEventListener('click', function (event) {
+  // textfield를 선택한 것이 아니게 만든다. => 모바일에 적용된 textfield 부분에 영향을 미치지 않도록 한다.
+  event.stopPropagation();
+  hideSearch();
+});
 searchShadowEl.addEventListener('click', hideSearch);
 
 function showSearch() {
   headerEl.classList.add('searching');
-  // html 문서 자체를 선택
-  document.documentElement.classList.add('fixed');
+  // html 문서 자체를 선택해서 스크롤이 실행되지 않도록 한다.
+  stopScroll();
   // .reverse() 사용하지 않고 원래 순서대로 반복 처리.
   headerMenuEls.reverse().forEach(function (el, index) {
     el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + 's';
@@ -64,7 +68,7 @@ function showSearch() {
 
 function hideSearch() {
   headerEl.classList.remove('searching');
-  document.documentElement.classList.remove('fixed');
+  playScroll();
   headerMenuEls.reverse().forEach(function (el, index) {
     el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + 's';
   });
@@ -74,6 +78,76 @@ function hideSearch() {
   searchDelayEls.reverse();
   // 나타날 때 원래의 순서대로 처리해야 하기 때문에 다시 뒤집어서 순서 돌려놓기!
   searchInputEl.value = ''; // 입력값 초기화
+}
+
+function playScroll() {
+  document.documentElement.classList.remove('fixed');
+}
+
+function stopScroll() {
+  document.documentElement.classList.add('fixed');
+}
+
+// 헤더 메뉴 토글! (mobile)
+const menuStaterEl = document.querySelector('header .menu-starter');
+menuStaterEl.addEventListener('click', function () {
+  if (headerEl.classList.contains('menuing')) {
+    headerEl.classList.remove('menuing');
+    searchInputEl.value = '';
+    playScroll();
+  } else {
+    headerEl.classList.add('menuing');
+    stopScroll();
+  }
+});
+
+// 헤더 검색 (mobile)
+const searchTextFieldEl = document.querySelector('header .textfield');
+const searchCancelEl = document.querySelector('header .search-canceler');
+searchTextFieldEl.addEventListener('click', function () {
+  headerEl.classList.add('searching--mobile');
+  searchInputEl.focus();
+});
+searchCancelEl.addEventListener('click', function () {
+  headerEl.classList.remove('searching--mobile');
+});
+
+//
+window.addEventListener('resize', function () {
+  if (window.innerWidth <= 740) {
+    headerEl.classList.remove('searching');
+  } else {
+    headerEl.classList.remove('searching--mobile');
+  }
+});
+
+//
+const navEl = document.querySelector('nav');
+const navMenuToggleEl = navEl.querySelector('.menu-toggler');
+const navMenuShadowEl = navEl.querySelector('.shadow');
+
+navMenuToggleEl.addEventListener('click', function () {
+  if (navEl.classList.contains('menuing')) {
+    hideNavMenu();
+  } else {
+    showNavMenu();
+  }
+});
+
+navEl.addEventListener('click', function (event) {
+  event.stopPropagation();
+});
+
+navMenuShadowEl.addEventListener('click', hideNavMenu);
+
+window.addEventListener('click', hideNavMenu);
+
+function showNavMenu() {
+  navEl.classList.add('menuing');
+}
+
+function hideNavMenu() {
+  navEl.classList.remove('menuing');
 }
 
 // 요소의 가시성 관찰
@@ -152,6 +226,7 @@ navigations.forEach(function (nav) {
   mapEl.innerHTML = /*html*/ `
     <h3>
         <span class="text">${nav.title}</span>
+        <span class="icon">+<span>
     </h3>
     <ul>
 ${mapList}
@@ -163,3 +238,12 @@ ${mapList}
 
 const thisYearEl = document.querySelector('span.this-year');
 thisYearEl.textContent = new Date().getFullYear();
+
+// 푸터 내비게이션 맵 아코디언
+const mapEls = [...document.querySelectorAll('footer .navigations .map')];
+mapEls.forEach((el) => {
+  const h3El = el.querySelector('h3');
+  h3El.addEventListener('click', () => {
+    el.classList.toggle('active');
+  });
+});
